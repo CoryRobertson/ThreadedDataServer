1c456cbd54db0797d5739f4aff30177683eaab2b
coryrobertson <cory.robertson17@gmail.com>
2022-04-29 18:11:00 -0700

    Implemented laser toggling with the F1 key

--- src/main/java/com/github/coryrobertson/ThreadedDataServer/ReverseServer/WebHost.java
+++ src/main/java/com/github/coryrobertson/ThreadedDataServer/ReverseServer/WebHost.java
@@ -1,20 +1,27 @@
 package com.github.coryrobertson.ThreadedDataServer.ReverseServer;
 
 import com.github.coryrobertson.ThreadedDataServer.Client.MousePosition;
-
-import java.util.Scanner;
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
 /**
  * A web host that hosts the entire data to be sent to the other device, in this case sending angle values to a rasperry pi
  */
-public class WebHost
+public class WebHost implements NativeKeyListener
 {
+
+    public static boolean laserState = false;
 
 
     public static void main(String[] args)
     {
+
         System.out.println("hosting web page on port 8111...");
         Thread reverseServerThread = new Thread(new ThreadedReverseServer());
+
+        globalHookKeyboard();
 
         reverseServerThread.start();
 
@@ -22,17 +29,42 @@ public class WebHost
 
         MousePosition mousePosition = new MousePosition();
 
-        String angleString = "";
+        String webTextInput = "";
+
 
         while(true)
         {
             int servoXDegrees = (int) mousePosition.map(mousePosition.getMapRangeX(),-1,1,0,180); // here we are mapping the mouse position from a range of -1 to 1, to a range of 0 to 180
             int servoYDegrees = (int) mousePosition.map(mousePosition.getMapRangeY(),-1,1,0,180);
-            angleString = servoXDegrees + "," + servoYDegrees; // we then append the angles to a string comma separated
+            webTextInput = servoXDegrees + "," + servoYDegrees; // we then append the angles to a string comma separated
+            webTextInput += "," + laserState;
 
-            ThreadedReverseServer.webText = angleString; // and we post it to the web page
+            ThreadedReverseServer.webText = webTextInput; // and we post it to the web page
         }
 
+    }
+
+    private static void globalHookKeyboard()
+    {
+        try { // setting up our keyboard listener
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            e.printStackTrace();
+        }
+        GlobalScreen.addNativeKeyListener(new WebHost());
+    }
+
+    public void nativeKeyPressed(NativeKeyEvent e)
+    {
+        if (e.getKeyCode() == NativeKeyEvent.VC_F1)
+        {
+            laserState = !laserState;
+            System.out.println("toggled laser");
+        }
+        if (e.getKeyCode() == NativeKeyEvent.VC_F3)
+        {
+            //pauseEffects = !pauseEffects;
+        }
     }
 
 }
--- src/main/python/servoPageReader.py
+++ src/main/python/servoPageReader.py
@@ -8,6 +8,14 @@ servo_kit_running = False
 
 try:
     from adafruit_servokit import ServoKit
+    import board
+    import busio
+    import adafruit_pca9685
+
+    i2c = busio.I2C(board.SCL, board.SDA)
+    hat = adafruit_pca9685.PCA9685(i2c)
+    hat.frequency = 60
+    led_channel = hat.channels[2]
 
     kit = ServoKit(channels=16)
     servo_kit_running = True
@@ -20,29 +28,40 @@ except:
 # print(args)
 
 def main():
-
     args = sys.argv
     print(args)
     # input_from_server = args[1]
+    arguments = []
     angles = []
     while 1:
         sleep(0.0125)
+        angles.clear()
         try:
-            #page successfully loaded
+            # page successfully loaded
             page = urllib.request.urlopen(args[1])
             pageText = str(page.read())
             conv = str(pageText[2:pageText.rfind('\'')])
-            angles = conv.split(",")
+            arguments = conv.split(",")
         except:
-            #if we are here the web page was most likely unable to load for a brief amount of time, we can just try again
+            # if we are here the web page was most likely unable to load for a brief amount of time, we can just try again
             print("web page dropped")
-        #print(angles)
+        # print(arguments)
+        angles.append(arguments[0])  # arg 0 is angle 0 of servo 0
+        angles.append(arguments[1])  # and so on
         change_servo_angles(angles)
+        if servo_kit_running == True:
+            if arguments[2] == "true":
+                print("laser is on!")
+                led_channel.duty_cycle = 65534
+            else:
+                print("laser is off!")
+                led_channel.duty_cycle = 0
+
 
 
 def change_servo_angles(angles):
-    #angle = clamp_angle(angle)
-    #servo_number = clamp_servo_selection(servo_number)
+    # angle = clamp_angle(angle)
+    # servo_number = clamp_servo_selection(servo_number)
     count = 0
     for angle in angles:
         if servo_kit_running:
@@ -50,10 +69,9 @@ def change_servo_angles(angles):
             kit.servo[clamp_servo_selection(count)].angle = int(clamp_angle(angle))
         else:
             print("UNCLAMPED servo number, angle: " + str(count) + ", " + str(angle) + " ")
-        count+=1
-    #print("servo number to change: " + str(servo_number) + " ")
-    #print("servo angle: " + str(angle))
-
+        count += 1
+    # print("servo number to change: " + str(servo_number) + " ")
+    # print("servo angle: " + str(angle))
 
 
 def clamp_angle(angle):
